a2b85c877131c4e3de9d796a8da652c7e1884f6c
parkjunghoon <go9018@naver.com>
2019-09-02 17:01:23 +0900

    add : network setting , retrofit

---
+++ src/client/app/src/main/java/com/seoul/app/zeropay_client/network/request/LoginRequest.kt
@@ -0,0 +1,3 @@
+package com.seoul.app.zeropay_client.network.request
+
+data class LoginRequest(var id: String, val pwd: String)
---
+++ src/client/app/src/main/java/com/seoul/app/zeropay_client/network/request/SearchIdRequest.kt
@@ -0,0 +1,3 @@
+package com.seoul.app.zeropay_client.network.request
+
+data class SearchIdRequest(val name: String, val phone: String)
